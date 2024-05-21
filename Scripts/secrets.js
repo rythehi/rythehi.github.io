fa8376006cd6dfebcb4066c4de135d1d3a7741f3
rythehi <rylanjarboe8@gmail.com>
2024-05-21 15:33:16 -0500

    newstuff

--- Scripts/secrets.js
+++ Scripts/secrets.js
@@ -1,10 +1,70 @@
-window.onload = function(){
-    var load = localStorage.getItem('secretload') || 1;
+window.onload = function() {
+    console.log('started');
+    const tut = document.getElementById('tut');
+    const p = document.getElementById('tutp');
+    // don't un commment
+    //tut.style.display = 'block';
+    //d3fu4lt('Finaly i have been here waiting forever');
+    localStorage.setItem('secretload', 0)
+    var load = localStorage.getItem('secretload') || 0;
     console.log(load);
-    if (load == 0){
+// condition load == 0
+    if (true) {
         console.log('if triggered');
-        const tut = document.getElementById('tut');
         tut.style.display = 'block';
-        localStorage.setItem('secretload', 1)
+        localStorage.setItem('secretload', 1);
+        localStorage.setItem('unlocked', JSON.stringify([{name:'finalearth2', label:'slope'}]))
+        d3fu4lt('Finally I have been here waiting forever (click to continue)');
+        var defualt_num = 0;
+        tut.addEventListener('click', function() {
+            console.log('event triggered');
+            if (defualt_num == 0){
+                d3fu4lt("This is all the secret games and pages behind that other stuff");
+                defualt_num = 1;
+            }
+            else if (defualt_num == 1){
+                d3fu4lt("Oh! and my name is d3fu4lt im the hacker in CWA that hacked the CIA in 2016, i think it was 2016?");
+                defualt_num = 2;
+            }
+            else if (defualt_num == 2){
+                d3fu4lt("i'm here to tell you about all the games you can unlock and cool apps and show you around.");
+                defualt_num = 3;
+            }
+            else if (defualt_num == 3){
+                d3fu4lt("behind me is where you can find all the games you have found.");
+                defualt_num = 4;
+            }
+            else if (defualt_num == 4){
+                d3fu4lt("and to the <---- left thers is a bar for hints and to change to your apps list.");
+                defualt_num = 5;
+            }
+            else if (defualt_num == 5){
+                d3fu4lt("Their are lots of cool things to discover and lots of useful apps!");
+                defualt_num = 6;
+            }
+            else if (defualt_num == 6){
+                d3fu4lt("Here is your first game, Until next time!!!");
+                defualt_num = 7;
+            }
+            else if (defualt_num == 7){
+                tut.style.display = 'none'
+                defualt_num = 6;
+            }
+        });
     }
+}
+function d3fu4lt(message) {
+    const p = document.getElementById('tutp');
+    let i = 0;
+    document.getElementById('character').textContent = 'd3fu4lt:';
+
+    function displayNextCharacter() {
+        if (i < message.length) {
+            p.textContent = message.slice(0, i + 1);
+            i++;
+            setTimeout(displayNextCharacter, 30); // Adjust the delay as needed (in milliseconds)
+        }
+    }
+
+    displayNextCharacter();
 }
---
+++ newgames/scripts/games.js
@@ -0,0 +1,59 @@
+
+var game = JSON.parse(localStorage.getItem('unlocked'));
+if (game !== null && typeof game !== 'undefined') {
+    console.log('ok!');
+} else {
+    console.log("myArray is null or undefined.");
+    localStorage.setItem('unlocked', JSON.stringify([{name:'finalearth2', label:'slope'}]))
+    game = JSON.parse(localStorage.getItem('unlocked'));
+}
+for (let i = 0; i < game.length; i++) {
+    console.log('starting to unlock games....')
+    console.log(game[i].name, game[i].label)
+    unlock(game[i].name, game[i].label);
+}
+function unlock(name, label){
+    const games = document.getElementById('games');
+    const ele = document.createElement('div');
+    const elep = document.createElement('p');
+    ele.setAttribute('class', 'icon');
+    ele.setAttribute('onclick', name + '()')
+    console.log(name + ' onclick is = ' + name + '()')
+    elep.setAttribute('class', 'iconp');
+    elep.textContent = label;
+    games.appendChild(ele);
+    ele.appendChild(elep);
+}
+//game functions (keep seperate)
+function finalearth2(){
+    const app = document.getElementById("app");
+    app.src = "games/the final earth/index.html";
+    document.getElementById('sidebar').style.display = 'none';
+    document.getElementById('main').style.display = 'none';
+    app.style.display = "block";
+    document.getElementById('x').style.display = "block";
+    close = 1;
+}
+function slope3(){
+    const app = document.getElementById("app");
+    app.src = "games/slope 3/index.html";
+    document.getElementById('sidebar').style.display = 'none';
+    document.getElementById('main').style.display = 'none';
+    app.style.display = "block";
+    document.getElementById('x').style.display = "block";
+    close = 1;
+}
+function closea(){
+    if (close == 1) {
+        const parent = document.getElementById("parent");
+        const app = document.getElementById("app");
+        app.style.display = "none";
+        document.getElementById('sidebar').style.display = 'block';
+        document.getElementById('main').style.display = 'block';
+        app.src = "";
+    }
+    if (close == 0){
+        window.location.href = "https://rythehi.github.io";
+    }
+    close = 0;
+}
